1d9ee23cd2b0b1b114222ad8446672116deca25f
Suluzhan <114818182+sisi00si@users.noreply.github.com>
2023-10-04 23:56:33 +0600

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,30 +8,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // Create the appointment scheduler instance
         AppointmentScheduler scheduler = AppointmentScheduler.getInstance();
 
-        // Create some appointments
         Appointment appointment1 = new Appointment("2023-10-10", "10:00 AM", "General Checkup");
         Appointment appointment2 = new Appointment("2023-10-12", "02:30 PM", "Dental Cleaning");
 
-        // Set the initial appointment strategy (basic strategy in this case)
         AppointmentStrategy basicStrategy = new BasicAppointmentStrategy((ArrayList<Object>) new ArrayList<>());
         scheduler.setAppointmentStrategy(basicStrategy);
 
-        // Schedule appointments using the scheduler with the basic strategy
         scheduler.scheduleAppointment(appointment1);
         scheduler.scheduleAppointment(appointment2);
 
-        // View scheduled appointments using the basic strategy
         System.out.println("Scheduled Appointments:");
         scheduler.viewAppointments();
 
-        // Cancel an appointment
-        int appointmentIdToCancel = 1; // Replace with the actual appointment ID to cancel
+        int appointmentIdToCancel = 1; 
         scheduler.cancelAppointment(appointmentIdToCancel);
 
-        // View updated appointments after cancellation
         System.out.println("\nAppointments after Cancellation:");
         scheduler.viewAppointments();
     }
